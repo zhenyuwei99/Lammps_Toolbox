6164872f02869f6d838835ce305df2a507f05f9e
zhenyuwei99 <zhenyuwei99@gmail.com>
2019-12-27 13:20:05 +0800

    0.0.9

--- Develop_test_file/test_readdump.m
+++
@@ -1,9 +0,0 @@
-clear all
-clc
-
-dump_name = '/Users/zhenyuwei/MD_simulation/dump.obstacle';
-dump_prop = ['id type coord_scl'];
-dump_col = [1 2 3];
-t_sim = 2;
-
-data = LammpsDataReadDump(dump_name,dump_prop,dump_col,t_sim);
